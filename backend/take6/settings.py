d3b805f899f25210a9f315ed21fb754124cc00bb
YMJ1123 <88578309+YMJ1123@users.noreply.github.com>
2025-05-24 15:25:38 +0800

    ALLOWED_HOSTS setting

--- backend/take6/settings.py
+++ backend/take6/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dljq&zl47_assiqa-w-@sl(p^_9aq3x_dp@9z@xoilv3uycx12'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["team-14-take-6.onrender.com"]
 
 
 # Application definition
