3f238ebd4da18f85a224347b347fdc56686a21e7
YMJ1123 <88578309+YMJ1123@users.noreply.github.com>
2025-05-24 23:55:33 +0800

    RoomList update

--- backend/take6/settings.py
+++ backend/take6/settings.py
@@ -92,11 +92,20 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  "default": dj_database_url.config(
+    default="sqlite:///" + str(BASE_DIR / "db.sqlite3"),
+    conn_max_age=600,
+    ssl_require=True,
+  )
 }
 
 
@@ -156,7 +165,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "https://team-14-take-6.onrender.com",
     "https://take6.netlify.app",
-    "https://take6.netlify.app/*",
+    # "https://take6.netlify.app/*",
 ]
 CORS_ALLOW_CREDENTIALS = True  # ✅ 允許跨域請求附帶認證信息（cookies）
 CORS_ALLOW_HEADERS = [
