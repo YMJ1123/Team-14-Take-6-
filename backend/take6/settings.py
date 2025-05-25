73c65e4c17704e4663903a3574444df89f172eef
YMJ1123 <88578309+YMJ1123@users.noreply.github.com>
2025-05-26 00:54:06 +0800

    PlanA Internal Database URL

--- backend/take6/settings.py
+++ backend/take6/settings.py
@@ -102,7 +102,7 @@ import dj_database_url
 DATABASES = {
   "default": dj_database_url.config(
     default="sqlite:///" + str(BASE_DIR / "db.sqlite3"),
-    conn_max_age=600,
+    conn_max_age=300,
     ssl_require=True,
   )
 }
